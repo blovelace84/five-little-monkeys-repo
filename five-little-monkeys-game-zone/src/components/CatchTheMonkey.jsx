89c63925bd2f35395f0b4c93c17a39e47cd4ff0c
blovelace84 <blovelace84@gmail.com>
2025-06-02 05:10:35 -0400

    Added a button go back to the titlescreen

--- five-little-monkeys-game-zone/src/components/CatchTheMonkey.jsx
+++ five-little-monkeys-game-zone/src/components/CatchTheMonkey.jsx
@@ -1,11 +1,13 @@
 'use client';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function CatchTheMonkey() {
   const [score, setScore] = useState(0);
   const [monkeyPos, setMonkeyPos] = useState({ top: '50%', left: '50%' });
   const [gameOver, setGameOver] = useState(false);
   const [timeLeft, setTimeLeft] = useState(30);
+  const router = useRouter();
 
   useEffect(() => {
     if (timeLeft === 0) {
@@ -37,7 +39,11 @@ export default function CatchTheMonkey() {
     <div className="relative w-full h-screen bg-blue-200">
       <h1 className="text-3xl text-center font-bold mt-4">Catch the Monkey</h1>
       <p className="text-center mb-2">Score: {score} | Time Left: {timeLeft}s</p>
-
+      <button 
+        onClick={() => router.push('/')}
+        className='absolute top-4 left-4 bg gray-700 text-white px-4 py-2 rounded hover:bg-gray-900'>
+          ⬅️ Back to Title
+        </button>
       {!gameOver ? (
         <img
           src="/images/monkey.png"
