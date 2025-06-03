719e8ce7c8e5c78d18f24a6e3b7202087bb31a08
blovelace84 <blovelace84@gmail.com>
2025-06-03 05:30:26 -0400

    working on a background image for title screen (page.js)

--- five-little-monkeys-game-zone/src/app/page.js
+++ five-little-monkeys-game-zone/src/app/page.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return(
-    <main className="min-h-screen flex flex-col items-center justify-center bg-yellow-100">
+    <main className="w-full h-screen flex flex-col justify-center items-center"
+          style={{ backgroundImage: "url('/images/title-background.jpg')" }}>
       <h1 className="text-4xl font-extrabold mb-8 text-purple-800 dropt-shadow-lg text-center">Five Little Monkeys Game Zone</h1>
       <div className="space-y-4">
         <Link href="/catch" className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
