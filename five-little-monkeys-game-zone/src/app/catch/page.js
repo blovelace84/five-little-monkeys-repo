f31c09f91fe53d1f61c637f93c4176c876e5c4b4
blovelace84 <blovelace84@gmail.com>
2025-06-16 19:50:05 -0400

    delete

--- five-little-monkeys-game-zone/src/app/catch/page.js
+++
@@ -1,9 +0,0 @@
-import CatchTheMonkey from '../../components/CatchTheMonkey';
-
-export default function CatchPage() {
-    return(
-        <main className='min-h-screen bg-yellow-100 flex flex-col items-center justify-center'>
-            <CatchTheMonkey />
-        </main>
-    );
-}
