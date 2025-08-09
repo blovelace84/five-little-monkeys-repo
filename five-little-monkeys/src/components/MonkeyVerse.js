e15642d3d2d4c59377562ba1c010b159314bf304
Brittani Lovelace <blovelace84@gmail.com>
2025-08-09 07:21:36 -0400

    Added song and fixing MonkeyVerse component

--- five-little-monkeys/src/components/MonkeyVerse.js
+++ five-little-monkeys/src/components/MonkeyVerse.js
@@ -1,14 +1,18 @@
-export default function MonkeyVerse() {
-    const text = 
+export default function MonkeyVerse({ monkeys = 0 }) {
+  const text =
     monkeys > 0
-        ? `${monkeys} little monkey${monkeys > 1 ? "s" : ""} jumping on the bed,`
-        : "No more monkeys jumping on the bed!";
-    const nextLine = monkeys > 0 `One fell off and bumped his head. : `
-        `Mama called the doctor and the doctor said...`;
-    return(
-        <div>
-            <p className="verse">{text}</p>
-            <p className="small">{nextLine}</p>
-        </div>
-    )
+      ? `${monkeys} little monkey${monkeys > 1 ? "s" : ""} jumping on the bed`
+      : "No more monkeys jumping on the bed!";
+
+  const nextLine =
+    monkeys > 0
+      ? "One fell off and bumped his head."
+      : "Mama called the doctor and the doctor said...";
+
+  return (
+    <div>
+      <p>{text}</p>
+      <p>{nextLine}</p>
+    </div>
+  );
 }
