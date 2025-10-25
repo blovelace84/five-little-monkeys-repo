b2c6833c115140a1c4724dbc6d6a6b877f437da8
Brittani Lovelace <blovelace84@gmail.com>
2025-10-25 16:48:42 -0400

    fixed my import issue

--- five-little-monkeys/src/components/MonkeyScene.jsx
+++ five-little-monkeys/src/components/MonkeyScene.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useMonkeyStore } from "../stores/monkeyStore.js";
-import { MonkeyCard } from "./MonkeyCard.jsx";
+import MonkeyCard from "../components/MonkeyCard.jsx";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function MonkeyScene() {
   const { monkeyCount, resetMonkeys } = useMonkeyStore();
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-linear--to-b from-blue-200 to-blue-400">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-b from-blue-200 to-blue-400">
       <h1 className="text-4xl font-bold mb-8 text-yellow-50 drop-shadow-lg">
         🐵 {monkeyCount} Little Monkeys Jumping on the Bed!
       </h1>
