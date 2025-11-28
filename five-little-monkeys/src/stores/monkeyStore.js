067fe097773b943a2fc91de8508a583ff8f0fca8
Brittani Lovelace <blovelace84@gmail.com>
2025-11-28 08:10:57 -0500

    delete

--- five-little-monkeys/src/stores/monkeyStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useMonkeyStore = create((set) => ({
-    monkeyCount: 5,
-    decreaseMonkey: () => set((state) => ({ monkeyCount: Math.max(0, state.monkeyCount - 1) })),
-    resetMonkeys: () => set({ monkeyCount: 5 }),
-}));
